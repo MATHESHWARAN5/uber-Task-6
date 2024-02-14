d3a4029013dc12937c97d4aa5188d0b2abd664bc
MATHESHWARAN5 <mangathamj24@gmail.com>
2024-02-14 11:04:38 +0530

    person updated

--- Uber.js
+++ Uber.js
@@ -1,23 +1,4 @@
-// Person class
-class Person {
-    private name: string;
-    private age: number;
-    private gender: string;
-  
-    constructor(name: string, age: number, gender: string) {
-      this.name = name;
-      this.age = age;
-      this.gender = gender;
-    }
-  
-    displayDetails(): void {
-      console.log(`Name: ${this.name}, Age: ${this.age}, Gender: ${this.gender}`);
-    }
-  }
-  
-  // Example usage of Person class
-  // const person = new Person("John Doe", 30, "Male");
-  // person.displayDetails();
+
   
   // UberPriceCalculator class
   class UberPriceCalculator {
